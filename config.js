1e287651a17f68f7bf24c73b931e8c17153d1043
MateusBicalho96 <mateusbicalho96@hotmail.com>
2024-05-27 19:06:32 -0300

    first commit

---
+++ config.js
@@ -0,0 +1,12 @@
+const dotenv = require('dotenv');
+
+dotenv.config();
+const {
+    PORT,
+    pgConnection
+} = process.env;
+
+module.exports = {
+    port: PORT,
+    urlConnection: pgConnection
+}
